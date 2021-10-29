b800fbfb79d2f25a0f96ed27eb6296050a95aa6c
Giovanni Malcolm <malcolm.giovanni@gmail.com>
2021-10-29 04:29:25 -0400

    store.js class and fucntions added

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# note-taker
+# Note Taker
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+       https://opensource.org/licenses/MIT
+
+  ## Description
+
+Simple note taker app to store and keep track of reminders.
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command: 
+
+```
+npm i
+```
+
+## Usage
+
+No special usage requirements.
+
+## License 
+
+This project is licensed under the MIT license.
+
+## Contributing 
+
+No special contribution requirements.
+
+## Tests
+
+To run tests, run the following command:
+
+```
+npm test
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at malcolm.giovanni@gmail.com. 
+You can find more of my work at ![giovannimalcolm](https://github.com/giovannimalcolm/).
--- db/store.js
+++ db/store.js
@@ -1,21 +1,54 @@
 const util = require('util');
-const fs = require('fs')
+const fs = require('fs');
 
-const readFileAysnc = util.promisify(fs.readFile)
-const writeFileAysnc = util.promisify(fs.writeFile)
+const uuidv1 = require('uuid/v1');
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-readData(){ 
-    return readFileAsync('db.json', 'utf8');
+  read() {
+    return readFileAsync('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+
+    const newNote = { title, text, id: uuidv1() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+  removeNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
 }
 
-writeData(note){
-    return writeFileAysnc('db.json', JSON.stringify(note));
-}
-
-
-
-
-
-}
+module.exports = new Store();
